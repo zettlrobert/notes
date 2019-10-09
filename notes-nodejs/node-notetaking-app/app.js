0e7b09705921f416941f05888587d4dc72fa184a
Robert Zettl <zettl.robert@gmail.com>
2019-10-09 20:55:00 +0200

    updated nodejs.md and useful-npm-packages, added add note and delete note to notetaking-application

--- notes-nodejs/node-notetaking-app/app.js
+++ notes-nodejs/node-notetaking-app/app.js
@@ -1,6 +1,68 @@
+//librarys
 const chalk = require('chalk');
-const getNotes = require('./notes.js');
+const yargs = require('yargs');
 
-const msg = getNotes();
-console.log(chalk.bold.green('Success!'));
-console.log(msg);
+//my files
+const notes = require('./notes.js');
+
+
+// Create Add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Node body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    notes.addNote(argv.title, argv.body)
+  }
+})
+
+
+// Create Remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    notes.removeNote(argv.title)
+  }
+})
+
+
+// Set up list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function() {
+    console.log('listing out all notes');
+  }
+})
+
+
+// Read Notes command
+yargs.command({
+  command: 'read',
+  describe: 'Read a Note',
+  handler: function() {
+    console.log('reading a note...');
+  }
+})
+
+
+yargs.parse();
